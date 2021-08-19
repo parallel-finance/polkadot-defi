12969c1492f8b0c6ff1c654e4041ad767e2e33fb
alvaro-ricotta <alvaro.e.ricotta@gmail.com>
2021-08-18 22:23:16 -0300

    updating subscribe form + ajax subscribe.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 var d 		  	= document,
     b         = d.querySelector('body'),
     menu      = d.querySelector(".submenu"),
+    submitForm  = d.getElementById('mc-embedded-subscribe'),
 		subMenu   = d.querySelector(".submenu__expanded");
 
 
@@ -24,3 +25,52 @@ function add_class_on_scroll() {
 function remove_class_on_scroll() {
     b.classList.remove("fade-in");
 }
+
+
+
+function ve(emails) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(emails).toLowerCase());
+}
+
+
+
+(function () {
+    document.getElementsByTagName('form')[0].addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        if (!ve(d.getElementById("mce-EMAIL").value)) { d.getElementById("mce-EMAIL").classList.add('error'); return}
+
+        if (!d.getElementById("group_1").checked) { d.getElementById("group_1").classList.add('error'); return}
+
+        
+
+        // Get url for mailchimp
+        var url = this.action.replace('/post?', '/post-json?');
+
+        // Add form data to object
+        var data = '';
+        var inputs = this.querySelectorAll('#newsletterform input');
+        for (var i = 0; i < inputs.length; i++) {
+            data += '&' + inputs[i].name + '=' + encodeURIComponent(inputs[i].value);
+        }
+
+        // Create & add post script to the DOM
+        var script = document.createElement('script');
+        script.src = url + data;
+        document.body.appendChild(script);
+
+        // Callback function
+        var callback = 'callback';
+        window[callback] = function(data) {
+
+            // Remove post script from the DOM
+            delete window[callback];
+            document.body.removeChild(script);
+
+            data.msg === 'Thank you for subscribing!' ? document.getElementsByTagName('form')[0].classList.add('sucess') : document.getElementById('js-subscribe-response').innerHTML = data.msg;
+
+            
+        };
+    });
+})();
